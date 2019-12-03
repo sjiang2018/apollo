7b31e98c7a77c32581fe5956b5bda8909735bf1e
sjiang2018 <shujiang2016@gmail.com>
2019-12-03 07:24:47 -0800

    Planning: using gflags for reuse path in lane_follow scenario

--- modules/planning/common/planning_gflags.cc
+++ modules/planning/common/planning_gflags.cc
@@ -512,3 +512,12 @@ DEFINE_double(side_pass_driving_width_l_buffer, 0.1,
 
 DEFINE_bool(use_st_drivable_boundary, false,
             "True to use st_drivable boundary in speed planning");
+
+DEFINE_bool(enable_prioritize_change_lane, false,
+            "True to enable prioritize change_lane reference line");
+
+DEFINE_bool(enable_remove_change_lane, false,
+            "True to enable remove change_lane reference line");
+
+DEFINE_bool(enable_reuse_path_in_lane_follow, false,
+            "True to enable reuse path in lane follow");
